2951d97628ee083ac39616e51fb2efef69b0f0cc
davidfekke <david@fekke.com>
2020-08-02 22:22:31 -0400

    Customizing starter site

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,20 +2,27 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+import Image from "../components/logoimage"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <h1>Welcome</h1>
+    <p>
+    This youtube channel is for software engineers and novice programmers alike to learn language features of a specific language when transitioning from one programming language to another.
+    </p>
+    <p>
+    Most software engineers do not use one programming language, but many, especially over the course of their career. It is important to be able to streamline the learning process by finding common features in a new language that you have used in other languages.
+    </p>
+    <p>
+    My goal is to create short videos around five minutes that can help software engineers learn just the feature they are looking for in a new language.      
+    </p>
+
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
 
