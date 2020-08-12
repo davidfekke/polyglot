4f6c3a8794d37fba58aa6a37822f1011022091f1
David Fekke <david@fekke.com>
2020-08-12 09:17:13 -0400

    Fixing the About and adding Playlist

--- src/pages/playlists.js
+++ src/pages/playlists.js
@@ -19,8 +19,16 @@ export default () => {
               <div style={{ display: 'block'}}>
                 <h1>Objective-C</h1>
                 <Youtubeplaylist list_id="PL1pnJRf1rXvib5fpvNOH0jXOeEK8NOrlm" />{/* 89F2V0jdA_g oY1eDJGtjxE */}
-                <p style={{ fontSize: '2rem'}}>
+                <p style={{ fontSize: '1rem'}}>
                 Check out the Objective-C series.
+                </p>
+              </div>
+
+              <div style={{ display: 'block'}}>
+                <h1>Javascript</h1>
+                <Youtubeplaylist list_id="PL1pnJRf1rXvg28q4ihJ-_0qs8A9DM7pCi" />{/* 89F2V0jdA_g oY1eDJGtjxE */}
+                <p style={{ fontSize: '1rem'}}>
+                These are some of the presentations on Javascript from the <ExtLink uri="https://www.jaxnode.com" name="JaxNode user group" />. 
                 </p>
               </div>
           </Article>
